03e11a74286955ff98d31cd337f84022599b40fc
Edmond Akwasi <edmondakwasi133@gmail.com>
2024-01-23 20:02:47 +0000

    'added-hero-section'

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React from "react";
 
 const Hero = () => {
   return (
+
     <div>
-      Hero
-      
+        <p>Welcome to Githubstreak</p>
+        <span>Level up your coding game, set new records, and revel in the camaraderie
+ of GitHub CodeStreak's Leaderboard</span>
     </div>
-  )
-}
+  );
+};
 
 export default Hero;
