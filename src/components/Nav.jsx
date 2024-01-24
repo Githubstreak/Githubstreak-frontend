06df723d02d0dfc125cf845a75c4d0920230227b
Edmond Akwasi <edmondakwasi133@gmail.com>
2024-01-24 20:32:15 +0000

    'updated-table'

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,69 +1,69 @@
-// import React from "react";
-// import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, DropdownItem, DropdownTrigger, Dropdown, DropdownMenu, Avatar} from "@nextui-org/react";
-// // import { headerLogo } from "./assets/images/Logo";
+import React from "react";
+import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, DropdownItem, DropdownTrigger, Dropdown, DropdownMenu, Avatar} from "@nextui-org/react";
+// import { headerLogo } from "./assets/images/Logo";
 
-// export default function App() {
-//   return (
-//     <Navbar>
-//       <NavbarBrand>
-//       {/* <Image 
-//             src={headerLogo}
-//             alt="Logo"
-//             width={120}
-//             height={29}
+export default function App() {
+  return (
+    <Navbar>
+      <NavbarBrand>
+      {/* <Image 
+            src={headerLogo}
+            alt="Logo"
+            width={120}
+            height={29}
         
-//         /> */}
-//         <p className="font-bold text-inherit">Githubstreak</p>
-//       </NavbarBrand>
+        /> */}
+        <p className="font-bold text-inherit">Githubstreak</p>
+      </NavbarBrand>
 
-//       <NavbarContent className="hidden sm:flex gap-4" justify="center">
-//         {/* <NavbarItem>
-//           <Link color="foreground" href="#">
-//             Features
-//           </Link>
-//         </NavbarItem> */}
-//         <NavbarItem isActive>
-//           <Link href="#" aria-current="page">
-//            Blog
-//           </Link>
-//         </NavbarItem>
-//         {/* <NavbarItem>
-//           <Link color="foreground" href="#">
-//             Integrations
-//           </Link>
-//         </NavbarItem> */}
-//       </NavbarContent>
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        {/* <NavbarItem>
+          <Link color="foreground" href="#">
+            Features
+          </Link>
+        </NavbarItem> */}
+        <NavbarItem isActive>
+          <Link href="#" aria-current="page">
+           Blog
+          </Link>
+        </NavbarItem>
+        {/* <NavbarItem>
+          <Link color="foreground" href="#">
+            Integrations
+          </Link>
+        </NavbarItem> */}
+      </NavbarContent>
 
-//       <NavbarContent as="div" justify="end">
-//         <Dropdown placement="bottom-end">
-//           <DropdownTrigger>
-//             <Avatar
-//               isBordered
-//               as="button"
-//               className="transition-transform"
-//               color="secondary"
-//               name="Jason Hughes"
-//               size="sm"
-//               src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
-//             />
-//           </DropdownTrigger>
-//           <DropdownMenu aria-label="Profile Actions" variant="flat">
-//             <DropdownItem key="profile" className="h-14 gap-2">
-//               <p className="font-semibold">Signed in as</p>
-//               <p className="font-semibold">zoey@example.com</p>
-//             </DropdownItem>
-//             <DropdownItem key="settings">Rank 23rd </DropdownItem>
-//             <DropdownItem key="team_settings">Achievements</DropdownItem>
-//             {/* <DropdownItem key="analytics"></DropdownItem>
-//             <DropdownItem key="system"></DropdownItem>
-//             <DropdownItem key="configurations"></DropdownItem> */}
-//             <DropdownItem key="help_and_feedback">Help & Feedback</DropdownItem>
-//             <DropdownItem key="logout" color="danger">
-//               Log Out
-//             </DropdownItem>
-//           </DropdownMenu>
-//         </Dropdown>
-//       </NavbarContent>
-//     </Navbar>
-//   );
-// }
+      <NavbarContent as="div" justify="end">
+        <Dropdown placement="bottom-end">
+          <DropdownTrigger>
+            <Avatar
+              isBordered
+              as="button"
+              className="transition-transform"
+              color="secondary"
+              name="Jason Hughes"
+              size="sm"
+              src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+            />
+          </DropdownTrigger>
+          <DropdownMenu aria-label="Profile Actions" variant="flat">
+            <DropdownItem key="profile" className="h-14 gap-2">
+              <p className="font-semibold">Signed in as</p>
+              <p className="font-semibold">zoey@example.com</p>
+            </DropdownItem>
+            <DropdownItem key="settings">Rank 23rd </DropdownItem>
+            <DropdownItem key="team_settings">Achievements</DropdownItem>
+            {/* <DropdownItem key="analytics"></DropdownItem>
+            <DropdownItem key="system"></DropdownItem>
+            <DropdownItem key="configurations"></DropdownItem> */}
+            <DropdownItem key="help_and_feedback">Help & Feedback</DropdownItem>
+            <DropdownItem key="logout" color="danger">
+              Log Out
+            </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+      </NavbarContent>
+    </Navbar>
+  );
+}
