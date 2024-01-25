d68e56943cd3ff0846cb3133af73d314e46bd3f2
Edmond Akwasi <107095324+Edmondakwasi@users.noreply.github.com>
2024-01-25 18:27:18 +0000

    Update Nav.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 
  const Nav = () => {
   return (
-            <Avatar
               isBordered
               as="button"
               className="transition-transform"
