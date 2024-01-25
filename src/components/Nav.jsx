40381be1d42aee298af002b48d73c7160429868b
Edmond Akwasi <107095324+Edmondakwasi@users.noreply.github.com>
2024-01-25 18:19:52 +0000

    Update Nav.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -8,7 +8,7 @@ export default function App() {
        
            
            
-          </Link>
+         
         </NavbarItem> */}
         <NavbarItem isActive>
           <Link href="#" aria-current="page">
