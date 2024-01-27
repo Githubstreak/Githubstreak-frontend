72ade98317cb3c3bb4c1b157fb0b3f7d6a15bb03
Edmond Akwasi <edmondakwasi133@gmail.com>
2024-01-27 23:00:07 +0000

    'added-hero-section'

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,4 +1,6 @@
-<footer className="footer footer-center p-10 bg-base-200 text-base-content rounded">
+const Footer = () => {
+  return (
+  <footer className="footer footer-center p-10 bg-base-200 text-base-content rounded">
   <nav className="grid grid-flow-col gap-4">
     <a className="link link-hover">About us</a>
     <a className="link link-hover">Contact</a>
@@ -16,3 +18,8 @@
     <p>Copyright © 2024 - All right reserved by ACME Industries Ltd</p>
   </aside>
 </footer>
+  )
+}
+
+export default Footer;
+
