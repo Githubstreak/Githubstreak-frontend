477536d6e76ae48e361bbf889d141e2b34d580db
Edmond Akwasi <edmondakwasi133@gmail.com>
2024-01-30 23:35:09 +0000

    'updated-file'

--- src/sections/Topthree.jsx
+++ src/sections/Topthree.jsx
@@ -1,6 +1,6 @@
 const Topthree = () => {
   return (
-      <div className="card w-96 bg-base-100 shadow-xl ">
+      <div className="card w-96 bg-base-100 shadow-xl">
         <figure className="px-10 pt-10">
             <img src="/" alt="profile" className="rounded-xl" />
         </figure>
