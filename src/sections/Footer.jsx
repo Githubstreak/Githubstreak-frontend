297a6f06adf1155264dd467f5474d842ac70f01b
Edmond Akwasi <edmondakwasi133@gmail.com>
2024-01-30 21:53:36 +0000

    'updated-footer'

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -4,8 +4,6 @@ const Footer = () => {
   <nav className="grid grid-flow-col gap-4">
     <a className="link link-hover">About us</a>
     <a className="link link-hover">Contact</a>
-    <a className="link link-hover">Jobs</a>
-    <a className="link link-hover">Press kit</a>
   </nav> 
   <nav>
     <div className="grid grid-flow-col gap-4">
