5fd63fa1abcd9bc6132b59878cb20553f05283bb
Edmond Akwasi <edmondakwasi133@gmail.com>
2024-02-01 01:09:48 +0000

    'updated-table'

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
   <footer className="footer footer-center p-10 bg-base-300 text-base-content rounded">
   <nav className="grid grid-flow-col gap-4">
-    <a href="/" className="link link-hover">About us</a>
+    <a href="Githubstreak-pdf" className="link link-hover">About us</a>
     <a className="link link-hover">Contact</a>
   </nav> 
   {/* <nav>
@@ -14,10 +14,13 @@ const Footer = () => {
       <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></a>
     </div>
   </nav>  */}
-  <aside>
-    <p class="mb-3">Copyright © 2024 - All right reserved #githubstreak '24</p>
+  <div className="flex-auto">
     <Avatar src="https://i.pravatar.cc/150?u=a042581f4e29026024d" />
     <p>Created by <a href="https://github.com/Edmondakwasi" >Edmond Akwasi</a></p>
+  </div>
+   
+    <aside>
+    <p class="mb-2">Copyright © 2024 - All right reserved #githubstreak '24</p>
   </aside>
 </footer>
   )
