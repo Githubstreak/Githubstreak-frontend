aff6aaf8eea948653a19fc30ac7c11b18ae04004
Edmond Akwasi <edmondakwasi133@gmail.com>
2024-02-02 19:13:25 +0000

    'updaated-table'

--- src/sections/Input.jsx
+++ src/sections/Input.jsx
@@ -3,8 +3,8 @@ import {Button} from "@nextui-org/react";
 
 const UserInput = () => {
   return (
-    <div className="flex w-70 p-10 flex-wrap md:flex-nowrap gap-4">
-      <Input type="email" label="username" placeholder="Enter your Github user name" />
+    <div className="flex  p-10 flex-wrap md:flex-nowrap gap-4">
+      <Input type="email" label="username" placeholder="Enter your Github user name"  />
       <Button color="success">
       Add me
     </Button>
