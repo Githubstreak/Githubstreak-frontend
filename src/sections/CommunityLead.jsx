da22157462c4449ecbb2e8bcaa1cb6f2a26e4715
Edmond Akwasi <edmondakwasi133@gmail.com>
2024-02-06 20:35:42 +0000

    'updated-table'

--- src/sections/CommunityLead.jsx
+++ src/sections/CommunityLead.jsx
@@ -3,22 +3,17 @@ import {User} from "@nextui-org/react";
 const CommunityLead = () => {
   return (
     <>
-    <div  class="p-">
-      <div class="flex justify-center"> 
-        <b>"Consistency Beats Talent"</b>
-      </div>
-        <div class="flex justify-center">
-          <p> The quick brown fox jumps over the lazy dog</p>
-        </div>
-    </div>
-    <User   
-      class="flex justify-center p-5"
+    <div  class="grid place-items-center p-10">
+      <b>"Consistency Beats Talent"</b>
+      <p  class="mb-5">The quick brown fox jumps over the lazy dog</p>
+    <User 
       name="Edmond Akwasi"
       description="Githubstreak Community Lead"
       avatarProps={{
         src: ""
       }}
     />
+    </div>
     </>
   );
 
