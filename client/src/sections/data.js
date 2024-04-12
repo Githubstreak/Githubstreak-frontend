cd8c4d5db94a04aee43ad0d4ddb6dab93e0408b9
Edmond Akwasi <107095324+Edmondakwasi@users.noreply.github.com>
2024-04-12 19:30:47 +0000

    Update data.js

--- client/src/sections/data.js
+++ client/src/sections/data.js
@@ -30,7 +30,7 @@ const users = [
     role: "Tech Lead",
     team: "Development",
     status: "paused",
-    streak: "56",
+    streak: "80",
     contributions: "11107",
     avatar: "https://i.pravatar.cc/150?u=a042581f4e29026704d",
     email: "Garby.lang@example.com",
