4b9c73e14493c80d490cf0c031de7d00cd579321
Edmond Akwasi <107095324+Edmondakwasi@users.noreply.github.com>
2024-04-22 21:53:02 +0000

    Update data.js

--- client/src/sections/data.js
+++ client/src/sections/data.js
@@ -44,7 +44,7 @@ const users = [
     streak: "97",
     contributions: "300",
     avatar: "https://i.pravatar.cc/150?u=a04258114e29026702d",
-    email: "Scott.fisher@example.com",
+    email: "Scott.fisher43@example.com",
   },
   {
     rank: 4,
