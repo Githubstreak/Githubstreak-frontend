afe131e3401bbf4177d7906085b6db28bb63fd5b
Edmond Akwasi <107095324+Edmondakwasi@users.noreply.github.com>
2024-04-24 20:08:32 +0000

    Update data.js

--- client/src/sections/data.js
+++ client/src/sections/data.js
@@ -216,7 +216,7 @@ const users = [
     role: "Administrator",
     team: "Information Technology",
     status: "paused",
-    streak: "34",
+    streak: "344",
     contributions: "290",
     avatar: "https://i.pravatar.cc/150?img=50",
     email: "rockson.harris@example.com",
