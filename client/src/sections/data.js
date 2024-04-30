38744a7b116a316154cf7962145f2d0be265dd25
Edmond Akwasi <107095324+Edmondakwasi@users.noreply.github.com>
2024-04-30 19:32:16 +0000

    Update data.js

--- client/src/sections/data.js
+++ client/src/sections/data.js
@@ -128,7 +128,7 @@ const users = [
     role: "Sr. Dev",
     team: "Development",
     status: "active",
-    streak: "32",
+    streak: "324",
     contributions: "29",
     avatar: "https://i.pravatar.cc/150?img=8",
     email: "abraham.stevens@example.com",
