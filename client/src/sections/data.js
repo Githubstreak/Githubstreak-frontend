52f189b4a53e69a87f0ad06237fc2ab9f360eeb0
Edmond Akwasi <107095324+Edmondakwasi@users.noreply.github.com>
2024-05-08 16:59:53 +0000

    Update data.js

--- client/src/sections/data.js
+++ client/src/sections/data.js
@@ -52,7 +52,7 @@ const users = [
     role: "CTO",
     team: "public speaking",
     status: "Active",
-    streak: "273",
+    streak: "278",
     contributions: "579",
     avatar: "https://i.pravatar.cc/150?u=a048581f4e29026701d",
     email: "Ekow.howard23@example.com",
