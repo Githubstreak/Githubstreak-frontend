706c7af0c8f283f22cbc66c53fafbbbc3fb0cb58
Edmond Akwasi <107095324+Edmondakwasi@users.noreply.github.com>
2024-05-27 23:19:55 +0000

    Update data.js

--- client/src/sections/data.js
+++ client/src/sections/data.js
@@ -85,7 +85,7 @@ const users = [
     role: "frontend  Developer",
     team: "Design",
     status: "paused",
-    streak: "1110",
+    streak: "1116",
     contributions: "897",
     avatar: "https://i.pravatar.cc/150?u=a042581f4e29027007d",
     email: "Iddrisu.hunt@example.com",
