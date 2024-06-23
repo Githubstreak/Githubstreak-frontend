2585666e57a7a35988bc2c72055913c46a80e512
Edmond Akwasi <107095324+Edmondakwasi@users.noreply.github.com>
2024-06-23 23:53:20 +0000

    Update data.js

--- client/src/sections/data.js
+++ client/src/sections/data.js
@@ -1,4 +1,4 @@
-8twuuconst columns = [
+const columns = [
   {name: "RANK", uid: "rank", sortable: true},
   {name: "DEVELOPER", uid: "developer"},
   {name: "STREAK", uid: "streak", sortable: true},
