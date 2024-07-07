beb3888229df4908b95eaa05490e5a8816cea40d
owbird <owbirdphorccuo@gmail.com>
2024-07-07 01:27:47 +0000

    feat (auth): Use github oauth

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,92 +1,30 @@
-import { useState } from 'react';
+import { Navbar, NavbarBrand, NavbarContent } from "@nextui-org/react";
 import {
-  Navbar,
-  NavbarBrand,
-  NavbarContent,
-  DropdownItem,
-  DropdownTrigger,
-  Dropdown,
-  DropdownMenu,
-  Avatar,
-  Button,
-} from "@nextui-org/react";
-import axios from 'axios'; // Import Axios for making HTTP requests
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+  useUser,
+} from "@clerk/clerk-react";
 
 export default function App() {
-  const [loggedIn, setLoggedIn] = useState(false); // State to track login status
-  const [user, setUser] = useState(null); // State to store user information
-
-  const handleLogin = async () => {
-    try {
-      // Redirect the user to the GitHub authentication page
-      window.location.href = "http://localhost:3000/auth/github";
-    } catch (error) {
-      console.error('Error logging in:', error);
-    }
-  };
-  
-  const LoginButton = () => {
-    return (
-      <Button color="success" onClick={handleLogin}>
-        Log In with GitHub
-      </Button>
-    );
-  };
-  
-
-
-  // Function to handle logout
-  const handleLogout = async () => {
-    try {
-      // Make a request to the backend to logout
-      const response = await axios.get('/logout');
-      // If the request is successful, update login status
-      if (response.status === 200) {
-        setLoggedIn(false);
-        setUser(null);
-      }
-    } catch (error) {
-      console.error('Error logging out:', error);
-    }
-  };
+  const { user } = useUser();
 
   return (
     <Navbar>
       <NavbarBrand>
-        <p className="font-extrabold text-3xl text-inherit text-green-900 sm: text-xl ">Githubstreak</p>
+        <p className="font-extrabold text-3xl text-inherit text-green-900">
+          Githubstreak
+        </p>
       </NavbarBrand>
 
       <NavbarContent as="div" justify="end">
-        {loggedIn ? (
-          <Dropdown placement="bottom-end">
-            <DropdownTrigger>
-              <Avatar
-                isBordered
-                as="button"
-                className="transition-transform"
-                color="success"
-                name={user.email} // Display user's email as their name
-                size="sm"
-                src={""} // Add user's profile picture if available
-              />
-            </DropdownTrigger>
-            <DropdownMenu aria-label="Profile Actions" variant="flat">
-              <DropdownItem key="profile" className="h-14 gap-2">
-                <p className="font-semibold">Signed in as</p>
-                <p className="font-semibold">{user.email}</p>
-              </DropdownItem>
-              <DropdownItem key="settings">Rank 2</DropdownItem>
-              <DropdownItem key="help_and_feedback">Help & Feedback</DropdownItem>
-              <DropdownItem key="logout" color="danger" onClick={handleLogout}>
-                Log Out
-              </DropdownItem>
-            </DropdownMenu>
-          </Dropdown>
-        ) : (
-          <Button color="success" onClick={handleLogin}>
-            Log In
-          </Button>
-        )}
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
       </NavbarContent>
     </Navbar>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { ClerkProvider } from "@clerk/clerk-react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const clerkPKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <ClerkProvider publishableKey={clerkPKey}>
+      <App />
+    </ClerkProvider>
   </React.StrictMode>,
-)
+);
