2b6bd06b1a5e5d88b14f16907f9fab68005f7eb6
owbird <owbirdphorccuo@gmail.com>
2024-07-12 12:45:07 +0000

    feat: Update community lead details

--- src/sections/CommunityLead.jsx
+++ src/sections/CommunityLead.jsx
@@ -1,25 +1,27 @@
-import {User} from "@nextui-org/react";
+import { User, Link } from "@nextui-org/react";
 
 const CommunityLead = () => {
   return (
     <>
-    <div className="grid place-items-center p-10 ">
-      <b className="mb-2 sm: text-2xl">"Consistency Beats Talent"</b>
-      <p className="mb-5 text-xl sm:text-medium" >Consistency beats talent
-      because it is more sustainable, adaptable, 
-      and rewarding. <br /> It helps one improve skills, 
-      achieve goals, and overcome challenges.</p>
-    <User 
-      name="Edmond Akwasi"
-      description="Githubstreak Community Lead"
-      avatarProps={{
-        src: ""
-      }}
-    />
-    </div>
+      <div className="grid place-items-center p-10 ">
+        <b className="mb-2 sm: text-2xl">"Consistency Beats Talent"</b>
+        <p className="mb-5 text-xl sm:text-medium">
+          Consistency beats talent because it is more sustainable, adaptable,
+          and rewarding. <br /> It helps one improve skills, achieve goals, and
+          overcome challenges.
+        </p>
+        <Link href={`https://x.com/edmond_akwasi`} size="sm" isExternal>
+          <User
+            name="Edmond Akwasi"
+            description="Githubstreak Community Lead"
+            avatarProps={{
+              src: "https://avatars.githubusercontent.com/u/107095324?v=4",
+            }}
+          />
+        </Link>
+      </div>
     </>
   );
-
-}
+};
 
 export default CommunityLead;
