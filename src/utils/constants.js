5f0e83036e0d29e13acb8c0c12d26fe8de6706e9
nicolello-dev <nicolamigone179@gmail.com>
2024-07-21 14:56:53 +0200

    Revert constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,3 @@
-export const API_URL = "https://githubstreak-backend.vercel.app";
+export const API_URL = import.meta.env.DEV
+  ? "http://localhost:3001"
+  : "https://githubstreak-backend.vercel.app";
