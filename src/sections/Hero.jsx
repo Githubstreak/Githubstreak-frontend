d4bb78d47280b9c008ad5a676ffa1d476280c642
Edmond Akwasi <107095324+Edmondakwasi@users.noreply.github.com>
2024-07-25 23:52:36 +0000

    updated the hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -14,25 +14,25 @@ const Hero = () => {
     >
       <div className="container mx-auto flex flex-col-reverse md:flex-row items-center justify-between h-full px-6 py-12">
         <div className="text-left md:w-1/2">
-          <h1 className="text-3xl font-bold md:text-6xl xl:text-8xl">
+          <h1 className="text-6xl font-bold md:text-6xl xl:text-8xl">
             Welcome to{" "}
-            <span className="text-green-700 font-extrabold">Githubstreak</span>
+            <span className="text-green-500  font-extrabold">Githubstreak</span>
           </h1>
-          <h3 className="text-4xl md:text-5xl font-bold text-white mb-4">
+          <h4 className="text-4xl font-semibold md:text-5xl text-white mt-4">
               Level up your coding game, and set new records!
-          </h3>
-          <h5 className="text-lg text-white mb-6">
-              Celebrate your achievements with our growing<br />GithubStreak Community!
+          </h4>
+          <h5 className="text-2xl text-white mb-6 mt-6">
+              Celebrate your achievements with our growing GithubStreak Community!
           </h5>
-          <h6 className="text-sm text-white mb-6">
-              We welcome all developers of any level or background.<br />Collaborate, learn, and grow together!
+          <h6 className="text-2xl text-white mb-6">
+              We welcome all developers of any level or background. Collaborate, learn, and grow together!
           </h6>
         </div>
         <div className="w-full md:w-auto h-auto md:max-w-xl lg:max-w-md xl:max-w-lg">
           <img
             src={summmary}
             alt="GitHub Streak Cards"
-            className="w-full md:w-auto h-100auto"
+            className="w-full  md:w-auto h-auto xl:max-w-lg"
           />
         </div>
       </div>
