d3004e26daf2d31da4d02cdb52a2a90d7b852dd9
agastya gaur <agastyagaur17@gmail.com>
2024-11-11 18:26:46 +0530

    refactor (footer): Improve code formatting and structure for better readability

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,42 +1,52 @@
-import { track } from '@vercel/analytics';
+import { track } from "@vercel/analytics";
 import { XLogo, YoutubeLogo, Discord } from "../components/icons";
-import logo from '/logo.png'
-import { Link } from 'react-router-dom';
+import logo from "/logo.png";
+import { Link } from "react-router-dom";
 const Footer = () => {
   return (
-
-<footer className="footer bg-neutral text-neutral-content items-center p-4">
- <div className=' w-full flex sm:flex-row flex-col items-center justify-between'>
- <aside className="flex  mx-auto sm:mx-0 items-center">
-    <img src={logo} alt="logo" className="  w-24 "/>
-    
-  </aside>
-  <div className=' ml-4'>
-  <p className=' font-semibold  mx-auto'>Copyright © {new Date().getFullYear()} - All right reserved #githubstreak '24</p>
-  </div>
-  <nav className=" flex gap-4  mx-auto sm:m-0 ">
-    <Link to={'https://x.com/githubstreak'} target="_blank" onClick={()=>{
-      track(<XLogo/>)
-    }}>
-      <XLogo/>
-    </Link>
-    <Link to={'https://youtube.com/@githubstreak01?si=tUr9EsmpuhAzgGad'} target="_blank" onClick={()=>{
-      track(<YoutubeLogo/>)
-    }}>
-     <YoutubeLogo/>
-    
-     
-    </Link>
-    <Link to={'https://discord.gg/BqrYjZZ9'} target="_blank" onClick={()=>{
-      track(<Discord/>)
-    }}>
-     <Discord/>
-    </Link>
-  </nav>
- </div>
-</footer>
-  )
-}
+    <footer className="footer bg-neutral text-neutral-content items-center p-4">
+      <div className=" w-full flex sm:flex-row flex-col items-center justify-between">
+        <aside className="flex  mx-auto sm:mx-0 items-center">
+          <img src={logo} alt="logo" className="  w-24 " />
+        </aside>
+        <div className=" ml-4">
+          <p className=" font-semibold  mx-auto">
+            Copyright © {new Date().getFullYear()} - All right reserved
+            #githubstreak '24
+          </p>
+        </div>
+        <nav className=" flex gap-4  mx-auto sm:m-0 ">
+          <Link
+            to={"https://x.com/githubstreak"}
+            target="_blank"
+            onClick={() => {
+              track(<XLogo />);
+            }}
+          >
+            <XLogo />
+          </Link>
+          <Link
+            to={"https://youtube.com/@githubstreak01?si=tUr9EsmpuhAzgGad"}
+            target="_blank"
+            onClick={() => {
+              track(<YoutubeLogo />);
+            }}
+          >
+            <YoutubeLogo />
+          </Link>
+          <Link
+            to={"https://discord.gg/BqrYjZZ9"}
+            target="_blank"
+            onClick={() => {
+              track(<Discord />);
+            }}
+          >
+            <Discord />
+          </Link>
+        </nav>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
-
